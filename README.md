0ff4af4ba897a97135af29f42020ae3edbfdfbde
Bojo966 <stil.2910@gmail.com>
2020-02-29 18:09:41 +0100

    Adding a proto file, server and updating readme'

--- README.md
+++ README.md
@@ -2,15 +2,35 @@
 
 ## Prerequisites
 
-- Download and install the latest version of Go
+- Download and install the latest version of [Go](https://golang.org/doc/install)
+- Make sure the following returns a result
+
+```bash
+echo $GOPATH
+```
+
 - Install gRPC by running the following:
 
 ```Bash
 go get -u google.golang.org/grpc
 ```
 
+- Install [protoc](https://developers.google.com/protocol-buffers) (platform dependent) and make sure you put it in your path
+
 - Install protococ plugin for Go:
 
 ```bash
 go get -u google.golang.org/grpc
+```
+
+For more info on gRPC with Go visit <https://grpc.io/docs/quickstart/go/>
+
+## Installation
+
+## Development
+
+### Gen files from proto files
+
+```bash
+
 ```
---
+++ server.go
@@ -0,0 +1,82 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+//go:generate protoc -I ../routeguide --go_out=plugins=grpc:../routeguide ../routeguide/route_guide.proto
+
+// Package main implements a simple gRPC server that demonstrates how to use gRPC-Go libraries
+// to perform unary, client streaming, server streaming and full duplex RPCs.
+//
+// It implements the route guide service whose definition can be found in routeguide/route_guide.proto.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/testdata"
+
+	pb "golang-todo-app/proto"
+)
+
+var (
+	tls        = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	certFile   = flag.String("cert_file", "", "The TLS cert file")
+	keyFile    = flag.String("key_file", "", "The TLS key file")
+	jsonDBFile = flag.String("json_db_file", "", "A json file containing a list of features")
+	port       = flag.Int("port", 10000, "The server port")
+)
+
+type routeGuideServer struct {
+	pb.UnimplementedTodoAppServer
+}
+
+func newServer() *routeGuideServer {
+	s := &routeGuideServer{}
+	return s
+}
+
+func main() {
+	log.Println("Starting up web server")
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	if *tls {
+		if *certFile == "" {
+			*certFile = testdata.Path("server1.pem")
+		}
+		if *keyFile == "" {
+			*keyFile = testdata.Path("server1.key")
+		}
+		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+		if err != nil {
+			log.Fatalf("Failed to generate credentials %v", err)
+		}
+		opts = []grpc.ServerOption{grpc.Creds(creds)}
+	}
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterTodoAppServer(grpcServer, newServer())
+	grpcServer.Serve(lis)
+}
